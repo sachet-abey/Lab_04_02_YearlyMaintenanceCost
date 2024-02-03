5f002267496a2d10ef01ec4a91aecc8bc61e38b8
sachet-abey <sachet.abeysinghe29@ihsd.us>
2024-02-03 16:11:17 -0500

    Lab 4 Task 2 Yearly Maintenance Cost

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double springCost = 100.15; // Initialize spring cost
+        double winterCost = 120.79; // Initialize winter cost
+        double summerCost = 115.00; // Initialize summer cost
+        double fallCost = 89.55; // Initialize fall cost
+        double totalMaintenanceCost = springCost + winterCost + summerCost + fallCost; // Calculate the total maintenance yearly costs
+        System.out.println("The total yearly maintenance costs for the seasonal costs "+springCost+" "+winterCost+" "+summerCost+" "+fallCost+" is "+totalMaintenanceCost); // Print results
     }
 }
